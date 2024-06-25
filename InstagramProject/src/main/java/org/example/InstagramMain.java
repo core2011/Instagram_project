ab51cb3bbd0b7b68fc101acc31c7e3d1cef4e593
Oleksandr Volkov <sashavolkov07@gmail.com>
2024-06-25 14:21:42 +0300

    first commit

---
+++ InstagramProject/src/main/java/org/example/InstagramMain.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import org.brunocvcunha.instagram4j.Instagram4j;
+
+public class InstagramMain {
+    public static void main(String[] args) {
+        Instagram4j instagram4j = Instagram4j.builder().username("d").password("").build();
+    }
+}
